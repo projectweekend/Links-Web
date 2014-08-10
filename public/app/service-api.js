0c2063a6b20d888e5b596dea551d1d98900022bc
Brian Hines <brian@projectweekend.net>
2014-08-10 16:17:23 -0500

    Add API service

--- public/app/service-api.js
+++ public/app/service-api.js
@@ -1,9 +1,48 @@
 var svcMod = angular.module( "linksWeb.service-api", [] );
 
 
-svcMod.factory( 'API', [ "$http", function ( $http ) {
+svcMod.factory( 'API', [ "$http", "$window", function ( $http, $window ) {
+
+    var apiRequest = function ( method, path, requestData, callback ) {
+
+        var headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Token " + $window.sessionStorage.token
+        };
+
+        var options = {
+            method: method,
+            url: path,
+            headers: headers,
+            data: requestData
+        };
+
+        $http( options )
+            .success( function ( data, status, headers, config ) {
+                callback( null, data );
+            } )
+            .error( function ( data, status, headers, config ) {
+                callback( data, null );
+            } );
+
+    };
 
     return {
+        $get: function ( path, callback ) {
+            return apiRequest( 'GET', path, {}, callback );
+        },
+        $post: function ( path, requestData, callback ) {
+            return apiRequest( 'POST', path, requestData, callback );
+        },
+        $put: function ( path, requestData, callback ) {
+            return apiRequest( 'PUT', path, requestData, callback );
+        },
+        $patch: function ( path, requestData, callback ) {
+            return apiRequest( 'PATCH', path, requestData, callback );
+        },
+        $delete: function ( path, callback ) {
+            return apiRequest( 'DELETE', path, {}, callback );
+        }
     };
 
 } ] );
