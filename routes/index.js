186f62d262bad5c3504144c6d76c48f3da7e4e6f
Brian Hines <brian@projectweekend.net>
2014-08-10 11:18:16 -0500

    Get server side routing setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+
+// Serve template partials for the Angular app
+router.get( '/partials/:name', function ( req, res ) {
+    res.render( 'partials/' + req.params.name );
+} );
+
+
+// Serve the index page for the Angular app
+router.get( '*', function ( req, res ) {
+    res.render( 'index' );
+} );
+
 
 module.exports = router;
