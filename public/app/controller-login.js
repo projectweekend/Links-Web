62200fcb9ee0fdb1c804384980e6961f20ff7bb1
Brian Hines <brian@projectweekend.net>
2014-08-10 19:58:31 -0500

    Start work on login

--- public/app/controller-login.js
+++ public/app/controller-login.js
@@ -1,5 +1,9 @@
 var ctlMod = angular.module( "linksWeb.controller-login", [] );
 
 
-ctlMod.controller( "Login", [ "$scope", function ( $scope ) {
+ctlMod.controller( "Login", [ "$scope", "API", function ( $scope, API ) {
+
+    $scope.username = "";
+    $scope.password = "";
+
 } ] );
