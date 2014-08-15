0051ed1fb07585dc0bf078ce5a941624e068e45a
Brian Hines <brian@projectweekend.net>
2014-08-15 05:55:09 -0500

    Initial effort on Signup controller

--- public/app/controller-signup.js
+++ public/app/controller-signup.js
@@ -1,9 +1,25 @@
 var ctlMod = angular.module( "linksWeb.controller-signup", [] );
 
 
-ctlMod.controller( "Signup", [ "$scope", function ( $scope ) {
+ctlMod.controller( "Signup", [ "$scope", "$rootScope", "$location", "API",
+    function ( $scope, $rootScope, $location, API ) {
 
-} ] );
+        $scope.signupData = {
+            username: "",
+            passowrd: ""
+        };
+
+        $scope.signup = function () {
+            API.$post( $rootScope.apiUrl + "/maker/register", $scope.signupData,
+                function ( err, data ) {
+                    if ( err ) {
+                        return $rootScope.$broadcast( 'error', data );
+                    }
+                    return $location.path( "/signup/confirm" );
+                } );
+        };
+
+    } ] );
 
 
 ctlMod.controller( "SignupConfirm", [ "$scope", function ( $scope ) {
