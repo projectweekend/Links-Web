ad0689dee5a81bf9595fede08aa7317284a5c122
Brian Hines <brian@projectweekend.net>
2014-08-17 08:47:21 -0500

    Rename signup scoped variable

--- public/app/controller-signup.js
+++ public/app/controller-signup.js
@@ -4,17 +4,19 @@ var ctlMod = angular.module( "linksWeb.controller-signup", [] );
 ctlMod.controller( "Signup", [ "$scope", "$rootScope", "$location", "API", "ENV",
     function ( $scope, $rootScope, $location, API, ENV ) {
 
-        $scope.signup = {
-            identifier: "",
+        $scope.newUser = {
+            email: "",
             passowrd: "",
             first_name: "",
             last_name: ""
         };
 
         $scope.signup = function () {
-            API.$post( ENV.apiRoot + "/maker/register", $scope.signup,
+            API.$post( ENV.apiRoot + "/maker/register", $scope.newUser,
                 function ( err, data ) {
                     if ( err ) {
+                        console.log( err );
+                        console.log( data );
                         return $rootScope.$broadcast( 'error', data );
                     }
                     return $location.path( "/signup/confirm" );
