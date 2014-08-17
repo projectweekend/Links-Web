0fa0b2ec2f38647cc3015c368cbdeb3046c0eb4c
Brian Hines <brian@projectweekend.net>
2014-08-17 17:06:03 -0500

    Handle server side errors on broadcast

--- public/app/controller-site.js
+++ public/app/controller-site.js
@@ -1,6 +1,27 @@
 var ctlMod = angular.module( "linksWeb.controller-site", [] );
 
 
-ctlMod.controller( "Site", [ "$rootScope", function ( $rootScope ) {
+ctlMod.controller( "Site", [ "$scope", "$rootScope", function ( $scope, $rootScope ) {
+
+    $scope.$on( 'error', function ( event, args ) {
+
+        $rootScope.errorMessages = [];
+
+        if ( args.errorData.status === 500 ) {
+            $rootScope.errorMessages.push( "A system error occurred" );
+            return;
+        }
+
+        var errorFields = Object.keys( args.errorData.data );
+        var errorDataObj = args.errorData.data;
+
+        for ( var ef = 0; ef < errorFields.length; ef++ ) {
+            for ( var em = 0; em < errorDataObj[ errorFields[ ef ] ].length; em++ ) {
+                var errorMessage = errorFields[ ef ] + ": " + errorDataObj[ errorFields[ ef ] ][ em ];
+                $rootScope.errorMessages.push( errorMessage );
+            }
+        }
+
+    } );
 
 } ] );
