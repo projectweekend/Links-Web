80a6af2dc2b67340c8fdf9521122e550ae8f5883
Brian Hines <brian@projectweekend.net>
2014-08-17 09:22:05 -0500

    Pass error to broadcast

--- public/app/controller-signup.js
+++ public/app/controller-signup.js
@@ -15,9 +15,7 @@ ctlMod.controller( "Signup", [ "$scope", "$rootScope", "$location", "API", "ENV"
             API.$post( ENV.apiRoot + "/maker/register", $scope.newUser,
                 function ( err, data ) {
                     if ( err ) {
-                        console.log( err );
-                        console.log( data );
-                        return $rootScope.$broadcast( 'error', data );
+                        return $rootScope.$broadcast( 'error', err );
                     }
                     return $location.path( "/signup/confirm" );
                 } );
